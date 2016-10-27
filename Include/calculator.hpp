f32f733827c58c709fc385530f09d9764ae28506
DianaHismatullina <dianarh@rambler.ru>
2016-10-27 21:18:27 +0300

    Update calculator.hpp

--- Include/calculator.hpp
+++ Include/calculator.hpp
@@ -1,1 +1,6 @@
-int sum(int a, int b);
+double summa(double a, double b)
+double sub(double a, double b)
+double mult(double a, double b)
+double dev(double a, double b)
+double pow(double a, double b)
+double sqrt(double a)
