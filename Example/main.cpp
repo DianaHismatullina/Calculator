b35c37bcd28123ba79a93a6585b48e6f37b92dd7
DianaHismatullina <dianarh@rambler.ru>
2016-10-27 21:23:28 +0300

    Update main.cpp

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-
 #include "calculator.hpp"
 
 int main()
 {
-    int x, y;
-    std::cin >> x;
-    std::cin >> y;
-    
-    std::cout << sum( x, y ) << std::endl;
+	double a, b;
+
+	printf_s("\n a = "); scanf_s("%lf", &a);
+	printf_s("\n b = "); scanf_s("%lf", &b);
+	printf_s("\n a + b =%f", summa(a, b));
+	printf_s("\n a - b =%f", sub(a, b));
+	printf_s("\n a * b =%f", mult(a, b));
+	printf_s("\n a / b =%f", dev(a, b));
+	printf_s("\n a ^ b =%f", pow(a, b));
+	printf_s("\n a^(1/2) =%f", sqrt(a));
+
+	return 0;
 }
