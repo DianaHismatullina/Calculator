53e674abc3cc36de574a54215f052afd70e94b17
DianaHismatullina <dianarh@rambler.ru>
2016-11-10 16:30:32 +0300

    Update main.cpp

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,32 +1,25 @@
-#include <iostream>
-#include "calculator.hpp"
-using namespace std;
+#include <iostream> 
+#include "calculator.hpp" 
 
-int main()
-{
-	double a, b;
-	
-	double summa(double a, double b);
-	double sub(double a, double b); 
-	double mult(double a, double b);
-	double dev(double a, double b);
-	double pow(double a, double b);
-	double sqrt(double a);
-    
-    setlocale(LC_ALL,"rus");
-    
-    cout<< "Введите значение a\n ";
-    cin >> a;
-    cout<< "Введите значение b\n ";
-    cin >> b;
-    
-    cout<<"\nCумма равна: " << summa(a,b);
-    cout<<"\nРазность равна: "<< sub(a,b);
-    cout<<"\nПроизведение равно: "<< pow(a,b);
-    cout<<"\nЧастное равно: "<< dev(a,b);
-    cout<<"\na в степени c равно: "<< mult(a,b);
-    cout<<"\nквадратный корень из c равен: "<<sqrt(a) ;
+int main() 
+{ 
+double a, b; 
 
-  
- system("pause");
+double summa(double a, double b); 
+double sub(double a, double b); 
+double mult(double a, double b); 
+double dev(double a, double b); 
+double pow(double a, double b); 
+double sqrt(double a); 
+
+printf_s("\n a = "); scanf_s("%lf", &a); 
+printf_s("\n b = "); scanf_s("%lf", &b); 
+printf_s("\n summa =%f", summa(a, b)); 
+printf_s("\n sub =%f", sub(a, b)); 
+printf_s("\n mult =%f", mult(a, b)); 
+printf_s("\n dev =%f", dev(a, b)); 
+printf_s("\n pow =%f", pow(a, b)); 
+printf_s("\n sqrt =%f", sqrt(a)); 
+
+return 0; 
 }
